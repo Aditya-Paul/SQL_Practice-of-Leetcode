a80790a7ed99548fe7daf2a74d603b07ed744bd9
Aditya Paul <102823756+Aditya-Paul@users.noreply.github.com>
2025-08-05 11:55:33 +0530

    Time: 1016 ms (39.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,52 @@
+/* Write your T-SQL query statement below */
+-- select product_id,new_price as price
+-- from(
+-- 	select *, DENSE_RANK() over (partition by product_id order by change_date desc) as rnk
+-- 	from Products
+-- 	where change_date <= '2019-08-16'
+-- 	)a
+-- where rnk = 1
+-- union all
+-- select product_id, case when new_price > 10 then '10' end as new_price
+-- from Products
+-- where product_id in (
+-- 					select product_id
+-- 					from Products
+-- 					where product_id not in (
+-- 												select product_id
+-- 												from(
+-- 												select *, DENSE_RANK() over (partition by product_id order by change_date desc) as rnk
+-- 												from Products
+-- 												where change_date <= '2019-08-16'
+-- 												)a
+-- 												where rnk = 1
+-- 											)
+-- 					)
+-- order by product_id
+
+select product_id, new_price as price
+from (
+    select *, DENSE_RANK() over (partition by product_id order by change_date desc) as rnk
+    from Products
+    where change_date <= '2019-08-16'
+) a
+where rnk = 1
+
+union
+
+select product_id, '10' as price
+from (
+    select product_id, max(new_price) as new_price
+    from Products
+    group by product_id
+) p
+where product_id not in (
+    select product_id
+    from (
+        select *, DENSE_RANK() over (partition by product_id order by change_date desc) as rnk
+        from Products
+        where change_date <= '2019-08-16'
+    ) a
+    where rnk = 1
+) and new_price > 10
+order by product_id;
