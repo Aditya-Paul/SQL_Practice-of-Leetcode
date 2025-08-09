9b7a690ac41e011e0bff4028b531dbe4f1d83424
Aditya Paul <102823756+Aditya-Paul@users.noreply.github.com>
2025-08-09 21:09:51 +0530

    Time: 1324 ms (50.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+select player_id, event_date as first_login
+from (
+select *,
+DENSE_RANK() over (partition by player_id order by event_date) as rnk
+from Activity ) t
+where rnk <2
