16066a151d177473178112c98ee3eebb4e16491c
Aditya Paul <102823756+Aditya-Paul@users.noreply.github.com>
2025-08-13 14:40:49 +0530

    Time: 967 ms (53.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select t.name,t.bonus
+from (
+		select em.empId,em.name,em.salary,bo.bonus
+		from Employee em
+		Left outer join Bonus bo
+		on em.empId = bo.empId
+	)as t
+where  t.bonus <1000 OR t.bonus IS NULL
